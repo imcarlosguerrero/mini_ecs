ffe0df9354f1c34b29612afd43323dc59a79c812
Steven Guerrero <imstevenguerrero@gmail.com>
2022-05-09 21:36:17 -0500

    Added Share Memory beta

--- elastic_container_service.c
+++ elastic_container_service.c
@@ -14,9 +14,23 @@ corren en el host interactuan con el proceso subscribe_host.
 #include<arpa/inet.h>	//inet_addr
 #include<unistd.h>	//write
 #include <time.h>
-
+#include <sys/mman.h>
 
 #define HOST_NUMBER 200
+
+void* create_shared_memory(size_t size) {
+  // Our memory buffer will be readable and writable:
+  int protection = PROT_READ | PROT_WRITE;
+
+  // The buffer will be shared (meaning other processes can access it), but
+  // anonymous (meaning third-party processes cannot obtain an address for it),
+  // so only this process and its children will be able to use it:
+  int visibility = MAP_SHARED | MAP_ANONYMOUS;
+
+  // The remaining parameters to `mmap()` are not important for this use case,
+  // but the manpage for `mmap` explains their purpose.
+  return mmap(NULL, size, protection, visibility, -1, 0);
+}
 
 int readHosts(){
 
@@ -148,6 +162,8 @@ int subscribe_host(){
 	readHosts();
 
 	int hostNumber = 0;
+
+	
 
 	int subscribe_host, client_sock, c, read_size;
 
@@ -351,15 +367,36 @@ int main() {
 
 	pid_t pid;
 
+	char parent_message[] = "hello";  // parent process will write this message
+
+  	char child_message[] = "goodbye"; // child process will then write this one
+
+  	void* shmem = create_shared_memory(128);
+
+  	memcpy(shmem, parent_message, sizeof(parent_message));
+
 	pid = fork();
 
 	if(pid == 0){
+
+		printf("Child read: %s\n", shmem);
+		memcpy(shmem, child_message, sizeof(child_message));
+		printf("Child wrote: %s\n", shmem);
+
+
+		//Subscribe_Host
 
 		subscribe_host();
 
 	}
 
 	else{
+
+		printf("Parent read: %s\n", shmem);
+		sleep(1);
+		printf("After 1s, parent read: %s\n", shmem);
+
+		//Admin_Container
 
 		admin_container();
 
